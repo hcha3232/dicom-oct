686a667b80952e27e638955873d1a5502614b33e
Hayden Chae <haydenchae98@gmail.com>
2025-07-14 20:44:53 +1000

    404

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+import { writeFileSync, readFileSync } from 'fs'
+
+
+function copyIndexTo404() {
+  return {
+    name: 'copy-index-to-404',
+    closeBundle() {
+      const indexPath = resolve(__dirname, 'dist/index.html')
+      const index = readFileSync(indexPath, 'utf-8')
+      writeFileSync(resolve(__dirname, 'dist/404.html'), index)
+    }
+  }
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/dicom-oct',
-  plugins: [react(), viteCommonjs(),tailwindcss(),],
+  plugins: [react(), viteCommonjs(),tailwindcss(),copyIndexTo404],
   optimizeDeps: {
     exclude: ['@cornerstonejs/dicom-image-loader'],
     include: ['dicom-parser'],
@@ -15,3 +29,5 @@ export default defineConfig({
     format: 'es',
   },
 })
+
+
